2405aae72d32f56195f105e86f87b87b532e3d95
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-06-29 18:50:10 -0300

    Condicionais - Parte 1

---
+++ CeV-js/Aula11/ex008.js
@@ -0,0 +1,9 @@
+console.log("O console funcionou corretamente");
+
+var vel = 68.2;
+
+console.log(`A velocidado do seu carro é de ${vel}Km/h.`);
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade máxima permitida. Multado!');
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ CeV-js/Aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'EUA';
+
+console.log(`Nasceu no ${pais}!`);
+
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro!');
+} else {
+    console.log('Você é estrangeiro!');
+}
