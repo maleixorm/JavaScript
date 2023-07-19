b94b8dfa291850214f3785ba980e122b82099455
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-07-19 07:38:36 -0300

    Condicionais - Parte 2

---
+++ CeV-js/Aula 12/ex012.js
@@ -0,0 +1,13 @@
+const agora = new Date();
+const hora = agora.getHours();
+
+console.log(`Agora são exatamente ${hora} horas.`);
+
+if (hora >= 6 && hora < 12) {
+    console.log('Bom dia.');
+} 
+else if (hora >= 12 && hora < 18) {
+    console.log('Boa tarde.');
+} else {
+    console.log('Boa noite.');
+}
