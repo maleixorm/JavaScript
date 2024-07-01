4812712f0a457479ed5fc60a23932a6232970c7d
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-01 16:13:46 -0300

    Manipulando vetores com funções próprias do JS

---
+++ arrays/array_incluir_remover.js
@@ -0,0 +1,20 @@
+var lista_frutas = [];
+
+lista_frutas[0] = 'banana';
+lista_frutas[1] = 'maça';
+
+
+// incluir elemento no final
+lista_frutas.push('uva');
+
+// incluir elemento no inicio
+lista_frutas.unshift('morango');
+
+
+// remover elemento no final
+lista_frutas.pop();
+
+// remover elemento no inicio
+lista_frutas.shift();
+
+console.log(lista_frutas);
