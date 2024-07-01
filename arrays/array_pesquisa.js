0f8dca773d38cfbdc807d08f8188ff48a71b5cbb
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-01 18:51:32 -0300

    Função para pesquisa em vetores no JS

---
+++ arrays/array_pesquisa.js
@@ -0,0 +1,8 @@
+var lista_frutas = [];
+
+lista_frutas[0] = 'banana';
+lista_frutas[1] = 'maça';
+lista_frutas[2] = 'morango';
+lista_frutas[3] = 'uva';
+
+console.log(lista_frutas.indexOf('morango'));
