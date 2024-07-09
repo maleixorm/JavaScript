d96859f5c789f119cb981d8ff928763144076f45
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-09 19:10:38 -0300

    Laço com o For In no JS

---
+++ estruturas/forin.js
@@ -0,0 +1,7 @@
+let listaConvidados = ['Jorge', 'Marcos', 'Jamilton', 'Ana', 'José', 'Maria'];
+
+console.log(listaConvidados);
+
+for (const x in listaConvidados) {
+    console.log('Indice ' + x + ' = ' + listaConvidados[x]);
+}
