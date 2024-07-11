eee188ea823aad07cba17eb3300063cf280ba90b
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-07-11 11:43:43 -0300

    Tratando erros com try, catch, finally e throw no JS

---
+++ tratandoErros.js
@@ -0,0 +1,24 @@
+var video = [];
+video[1] = [];
+video[1]['nome'] = 'Fullmetal Alchemist';
+video[1]['categoria'] = 'Anime';
+
+function getVideo(video) {
+    try {
+        console.log(video[0]['nome']);
+    } catch(erro) {
+        tratarErro(erro);
+        console.log('Agora podemos tratar o erro!');
+        throw new Error('Houve um erro mas não se preocupe. Estamos trabalhando nisso agora!');
+    } 
+    finally {
+        console.log('Sempre passa por aqui!');
+    }
+    console.log('A aplicação não morreu!');
+}
+
+function tratarErro (e) {
+    console.log(e);
+}
+
+getVideo(video);
